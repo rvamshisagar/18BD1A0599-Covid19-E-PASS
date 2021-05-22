36b7ee00dd2ddeae934160e04dcae0bdd8a47d8c
rvamshisagar <56620862+rvamshisagar@users.noreply.github.com>
2021-05-22 17:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import requests
 from flask import Flask, render_template, request, redirect
 from twilio.rest import Client
 
-account_sid = 'AC99a37f098378ebc68c73b99c751260d2'
-auth_token = 'd5a29b2cf2502c0801eddfa909afd81f'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 app = Flask(__name__, static_url_path='/static')
 
@@ -45,7 +45,7 @@ def registration_details():
     if travel_pass < 30:
         status = 'CONFIRMED'
         print("status",status)
-        client.messages.create(to="+91"+phonenumber, from_="+19733390680",
+        client.messages.create(to="+91"+phonenumber, from_="",
                               body="Hello" + " " + full_name + " " + "Permission for your Travel From" + " " + source_dt + " " + destination_dt + " Is GRANTED")
         return render_template('Registration_Details.html', var1=full_name, var2=email_id, var3=id_proof,
                                var4=source_st, var5=source_dt, var6=destination_st, var7=destination_dt,
@@ -53,7 +53,7 @@ def registration_details():
     else:
         status = 'NOT CONFIRMED'
         print("status:",status)
-        client.messages.create(to="+91"+phonenumber, from_="+19733390680",
+        client.messages.create(to="+91"+phonenumber, from_="",
                                body="Hello" + " " + full_name + " " + "Permission for your Travel From" + " " + source_dt + " " + destination_dt + " IS NOT GRANTED")
         return render_template('Registration_Details.html', var1=full_name, var2=email_id, var3=id_proof,
                                var4=source_st, var5=source_dt, var6=destination_st, var7=destination_dt,
